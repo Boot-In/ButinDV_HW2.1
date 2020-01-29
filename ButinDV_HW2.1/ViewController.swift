ea0dc06f149d43b63a4283711b588e0628fbf357
Butin <butin@rambler.ru>
2020-01-29 12:02:42 +1000

    Initial Commit

---
+++ ButinDV_HW2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ButinDV_HW2.1
+//
+//  Created by Dmitriy Butin on 29.01.2020.
+//  Copyright © 2020 Dmitriy Butin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
